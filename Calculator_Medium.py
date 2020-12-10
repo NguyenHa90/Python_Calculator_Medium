a92b2193ab89f8eb28b87878a86da79cef8d6011
Nguyễn Văn Hà <68882836+NguyenHa90@users.noreply.github.com>
2020-12-10 22:24:41 +0700

    Tính toán giá trị trung bình của mảng

--- Calculator_Medium.py
+++ Calculator_Medium.py
@@ -35,18 +35,9 @@ def calculator_medium_battery_value(value):
                     del list_value_battery[0]
                     list_value_battery.append(value)
                 break
-
-    mau_so = len(list_value_battery)
-    for k in list_value_battery:
-        tu_so = tu_so + k
-        # break
-    medium_battery = tu_so/mau_so
-    tu_so = 0
-    mau_so = 0
-    print (medium_battery)
+    medium_battery = float(sum(list_value_battery)) / max(len(list_value_battery), 1)
     return medium_battery
 while True:
     time.sleep(1)
     nValue += 1
-    print (list_value_battery)
     calculator_medium_battery_value(nValue)
